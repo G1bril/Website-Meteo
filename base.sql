9f1365494b25535ad0350ea0d74f8c23e448ee88
G1bril <157470588+G1bril@users.noreply.github.com>
2024-04-02 12:41:30 +0200

    ajout d'une section ville pour users

--- base.sql
+++ base.sql
@@ -1,8 +1,13 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS admins;
+  
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    ville VARCHAR(50)
 );
 
 CREATE TABLE admins (
